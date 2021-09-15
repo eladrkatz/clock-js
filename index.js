a989c2630a7019e8273abd19a304008194433ad2
EladRKatz <eladkt@gmail.com>
2021-09-15 17:39:33 +0300

    add clock

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var ctx = null;
 function window_loaded() {
     var theCanvas = document.getElementById('theCanvas');
     ctx = theCanvas.getContext('2d');
-    var degrees1 = 90;
+    var degrees1 = 360;
     ctx.beginPath();
     ctx.arc(300, 300, 200, 0, (degrees1 / 360) * (Math.PI * 2));
     ctx.fillStyle = 'red';
@@ -16,25 +16,40 @@ function window_loaded() {
     var radius = 200;
     var centerX = 300;
     var centerY = 300;
-    ctx.beginPath();
-    ctx.moveTo(centerX, centerY);
-    ctx.lineTo(centerX + radius, centerY);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(centerX, centerY);
-    ctx.lineTo(centerX, centerY + radius);
-    ctx.stroke();
-    var degrees = 10;
+    var degrees = 0;
     console.log();
-    for (var i = 0; i < 8; i++) {
-        var angle = (degrees / 360) * (Math.PI * 2);
-        var x = Math.cos(angle) * radius;
-        var y = Math.sin(angle) * radius;
-        ctx.beginPath();
-        ctx.moveTo(centerX, centerY);
-        ctx.lineTo(centerX + x, centerY + y);
-        ctx.stroke();
-        degrees = degrees + 10;
+    for (var i = 0; i < 60; i++) {
+        var angle_1 = (degrees / 360) * (Math.PI * 2);
+        var x = Math.cos(angle_1) * radius;
+        var y = Math.sin(angle_1) * radius;
+        if (i % 5 === 0) {
+            ctx.beginPath();
+            ctx.moveTo(centerX + x - x / 5, centerY + y - y / 5);
+            ctx.lineTo(centerX + x - x / 10, centerY + y - y / 10);
+            ctx.stroke();
+        }
+        else {
+            ctx.beginPath();
+            ctx.moveTo(centerX + x - x / 6, centerY + y - y / 6);
+            ctx.lineTo(centerX + x - x / 7, centerY + y - y / 7);
+            ctx.stroke();
+        }
+        degrees = degrees + 6;
     }
+    var hourDegrees = 30;
+    var angle = (hourDegrees * 7 / 360) * (Math.PI * 2);
+    var xTime = Math.cos(angle) * radius / 2.5;
+    var yTime = Math.sin(angle) * radius / 2.5;
+    ctx.beginPath();
+    ctx.moveTo(centerX, centerY);
+    ctx.lineTo(centerX + xTime, centerY + yTime);
+    ctx.stroke();
+    angle = (hourDegrees * 11 / 360) * (Math.PI * 2);
+    xTime = Math.cos(angle) * radius / 1.5;
+    yTime = Math.sin(angle) * radius / 1.5;
+    ctx.beginPath();
+    ctx.moveTo(centerX, centerY);
+    ctx.lineTo(centerX + xTime, centerY + yTime);
+    ctx.stroke();
 }
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ function window_loaded() {
 
     ctx = theCanvas.getContext('2d');
 
-     const degrees1 = 90;
+     const degrees1 = 360;
 
      ctx.beginPath();
      ctx.arc(300, 300, 200, 0, (degrees1 / 360) * (Math.PI * 2));
@@ -25,31 +25,50 @@ function window_loaded() {
     const centerX = 300;
     const centerY = 300;
 
-    ctx.beginPath();
+    
 
-    ctx.moveTo(centerX, centerY);
-    ctx.lineTo(centerX + radius, centerY);
-    ctx.stroke();
-
-    ctx.beginPath();
-
-    ctx.moveTo(centerX,centerY);
-    ctx.lineTo(centerX, centerY + radius);
-    ctx.stroke();
-
-    let degrees = 10;
+    let degrees = 0;
     console.log();
 
-    for(let i = 0; i < 8; i++)
-    {
+    for(let i = 0; i < 60; i++){
         let angle = (degrees / 360) * (Math.PI * 2);
         let x = Math.cos(angle) * radius;
         let y = Math.sin(angle) * radius;
-    
-        ctx.beginPath();
-        ctx.moveTo(centerX,centerY);
-        ctx.lineTo(centerX + x, centerY + y);
-        ctx.stroke();
-        degrees = degrees + 10;
+        
+        if(i % 5 === 0){
+            ctx.beginPath();
+            ctx.moveTo(centerX + x - x / 5 ,centerY + y - y / 5);
+            ctx.lineTo(centerX + x - x / 10 , centerY + y - y / 10);
+            ctx.stroke();
+        } else {
+            ctx.beginPath();
+            ctx.moveTo(centerX + x - x / 6 ,centerY + y - y / 6);
+            ctx.lineTo(centerX + x - x / 7 , centerY + y - y / 7);
+            ctx.stroke();
+        }
+
+
+        degrees = degrees + 6;
     }
+
+     const hourDegrees = 30;
+    let angle = (hourDegrees * 7 / 360) * (Math.PI * 2);
+    let xTime = Math.cos(angle) * radius / 2.5;
+    let yTime = Math.sin(angle) * radius / 2.5;
+
+    ctx.beginPath();
+    ctx.moveTo(centerX, centerY);
+    ctx.lineTo(centerX + xTime, centerY + yTime);
+    ctx.stroke();
+
+    angle = (hourDegrees * 11 / 360) * (Math.PI * 2);
+    xTime = Math.cos(angle) * radius / 1.5;
+    yTime = Math.sin(angle) * radius / 1.5;
+
+    ctx.beginPath();
+    ctx.moveTo(centerX, centerY);
+    ctx.lineTo(centerX + xTime, centerY + yTime);
+    ctx.stroke();
+
+
 }
